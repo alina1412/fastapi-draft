4d20126facff0dad274bb380e75e315ff3418dfa
alina1412 <alina1412@mail.ru>
2024-08-25 17:34:23 +0300

    sqlalchemy example

--- service/endpoints/put_handlers.py
+++ service/endpoints/put_handlers.py
@@ -1,7 +1,15 @@
+import random
 from fastapi import APIRouter, Depends, status
+
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from service.db_setup.db_settings import get_session
+from service.utils import (
+    delete_some_data,
+    get_some_data,
+    put_some_data,
+    update_some_data,
+)
 
 
 api_router = APIRouter(
@@ -18,6 +26,12 @@ api_router = APIRouter(
     },
 )
 async def put_data2(add_data=None, session: AsyncSession = Depends(get_session)):
-    """"""
-    print(add_data, session)
-    return {"data": "user_token_data"}
+    """example with postgres sqlalchemy"""
+    add_data = add_data if add_data else str(random.random())
+    print(add_data)
+    id_ = await put_some_data(session, {"name": add_data, "password": "bbb"})
+    users = await get_some_data(session)
+    print(users)
+    await update_some_data(session, {"id": 1, "active": 0})
+    await delete_some_data(session, {"id": 2})
+    return {"user_id": id_}
