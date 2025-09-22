320f7c7f12f024c64e0fd89142ffee055c993035
alina1412 <alina1412@mail.ru>
2025-09-22 14:22:24 +0300

    upd

--- tests/test_db_accessors.py
+++ tests/test_db_accessors.py
@@ -1,15 +1,11 @@
 import logging
 
-import pytest
-
 from service.db_accessors import UserAccessor
 
-pytest_plugins = ("pytest_asyncio",)
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 
-@pytest.mark.asyncio
 async def test_db_create(db):
     user_manager = UserAccessor(db)
     result = await user_manager.create_user(
--- tests/test_handlers.py
+++ tests/test_handlers.py
@@ -1,13 +1,8 @@
 import logging
 
-import pytest
-import pytest_asyncio
-
 pytest_plugins = ("pytest_asyncio",)
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
-
-pytestmark = pytest.mark.asyncio
 
 
 async def test_show_data_handler(client):
